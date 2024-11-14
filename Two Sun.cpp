72823521d08d773f883a9e0e61adc4d70e26b321
cubo <hpontello81@gmail.com>
2024-11-14 13:51:27 -0300

    add new coding from leetcode

--- Two Sun.cpp
+++ Two Sun.cpp
@@ -43,9 +43,19 @@ Only one valid answer exists.
 Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 */
 
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = i+1; j < nums.size(); j++){
+                if(nums[i] + nums[j] == target){
+                    return{i,j};
+                }
+            }
+        }
+    return {};
     }
 };
